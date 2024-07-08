5dfee9db433b3b9bedefe858f74daa1267277975
MichaelWest22 <michael@latent.nz>
2024-07-09 01:04:09 +1200

    Add InlineStyleNonce tests and CSP

--- test/core/nonce.js
+++ test/core/nonce.js
@@ -9,9 +9,23 @@ describe('Core CSP Nonce checks', function() {
     clearWorkArea()
   })
 
-  it('inlineScriptNonce sets nonce of script correctly so it can run when CSP nonce set', function(done) {
-    window.i = 0 // set count to 0
+  it('style with inlineStyleNonce set applies when CSP header set', function() {
+    // Note that it is hard to test the htmx inline indicator styles directly so create a testable style in head instead
+    const nonceAttribute = htmx.config.inlineStyleNonce ? ` nonce="${htmx.config.inlineStyleNonce}"` : ''
+    document.head.insertAdjacentHTML('beforeend', '<style' + nonceAttribute + '>.width-nonce{ width:150px;}</style>')
+    var div = make('<div id="d1" class="width-nonce" hx-get="/test"/>')
+    div.clientWidth.should.equal(150)
+  })
 
+  it('style without inlineStyleNonce fails to apply when CSP header set', function() {
+    document.head.insertAdjacentHTML('beforeend', '<style>.width-no-nonce{ width:150px;}</style>')
+    var div = make('<div id="d1" class="width-no-nonce" hx-get="/test"/>')
+    div.clientWidth.should.not.equal(150)
+  })
+
+  it('inlineScriptNonce sets nonce of script so it can run when CSP script nonce set', function(done) {
+    // Note inlineScriptNonce and CSP configured in index.html which applies to all tests
+    window.i = 0 // set count to 0
     this.server.respondWith('GET', '/test', '<script nonce="shouldBeReplaced">console.trace(); window.i++</script>') // increment the count by 1
     var div = make('<div hx-get="/test" hx-swap="innerHTML settle:5ms"/>')
     div.click()
@@ -27,7 +41,7 @@ describe('Core CSP Nonce checks', function() {
   it('inlineScriptNonce not set prevents inline scripts running because it will not match CSP', function(done) {
     window.i = 0 // set count to 0
     this.server.respondWith('GET', '/test', '<script nonce="shouldBeReplaced">console.trace(); window.i++</script>') // fail to increment the count by 1
-    htmx.config.inlineScriptNonce = ''
+    htmx.config.inlineScriptNonce = '' // disable inlineScriptNonce
     var div = make('<div hx-get="/test" hx-swap="innerHTML settle:5ms"/>')
     div.click()
     this.server.respond()
@@ -43,7 +57,7 @@ describe('Core CSP Nonce checks', function() {
   it('inlineScriptNonce set wrong prevents inline scripts running because it will not match CSP', function(done) {
     window.i = 0 // set count to 0
     this.server.respondWith('GET', '/test', '<script nonce="shouldBeReplaced">console.trace(); window.i++</script>') // fail to increment the count by 1
-    htmx.config.inlineScriptNonce = 'invalid'
+    htmx.config.inlineScriptNonce = 'invalid' // make inlineScriptNonce invalid
     var div = make('<div hx-get="/test" hx-swap="innerHTML settle:5ms"/>')
     div.click()
     this.server.respond()
@@ -57,9 +71,10 @@ describe('Core CSP Nonce checks', function() {
   })
 
   it('safeInlineScriptNonce set and HX-Nonce header match script nonce allows inline scripts to run', function(done) {
+    // safeInlineScriptNonce overrides inlineScriptNonce function and requires the server to respond with HX-Nonce header instead
     window.i = 0 // set count to 0
     this.server.respondWith('GET', '/test', [200, { 'HX-Nonce': '6p1zabP/K+va3O8bi2yydg==' }, '<script nonce="6p1zabP/K+va3O8bi2yydg==">console.trace(); window.i++</script>'])
-    htmx.config.safeInlineScriptNonce = 'nonce'
+    htmx.config.safeInlineScriptNonce = 'nonce' // set to match CSP script nonce
     var div = make('<div hx-get="/test" hx-swap="innerHTML settle:5ms"/>')
     div.click()
     this.server.respond()
@@ -75,7 +90,7 @@ describe('Core CSP Nonce checks', function() {
   it('safeInlineScriptNonce set wrong blocks inline scripts running', function(done) {
     window.i = 0 // set count to 0
     this.server.respondWith('GET', '/test', [200, { 'HX-Nonce': '6p1zabP/K+va3O8bi2yydg==' }, '<script nonce="6p1zabP/K+va3O8bi2yydg==">console.trace(); window.i++</script>'])
-    htmx.config.safeInlineScriptNonce = 'invalid'
+    htmx.config.safeInlineScriptNonce = 'invalid' // When set to an invalid value expect inline scripts to fail
     var div = make('<div hx-get="/test" hx-swap="innerHTML settle:5ms"/>')
     div.click()
     this.server.respond()
