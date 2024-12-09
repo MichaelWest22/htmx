e177688ac54a58b9cae79e4cba9e6ffde55b00dd
MichaelWest22 <michael@latent.nz>
2024-12-09 15:17:09 +1300

    Test fixing all invalid templates

--- src/htmx.js
+++ src/htmx.js
@@ -621,6 +621,29 @@ var htmx = (function() {
         fragment.querySelectorAll('script').forEach((script) => script.remove())
       }
     }
+
+    var templates = fragment.querySelectorAll('template');
+    var el, template, attribs, attrib, count, child, content;
+    for (var i=0; i<templates.length; i++) {
+      el = templates[i];
+      if (!el.content) {
+        template = el.ownerDocument.createElement('template');
+        el.parentNode.insertBefore(template, el);
+        attribs = el.attributes;
+        count = attribs.length;
+        while (count-- > 0) {
+          attrib = attribs[count];
+          template.setAttribute(attrib.name, attrib.value);
+          el.removeAttribute(attrib.name);
+        }
+        el.parentNode.removeChild(el);
+        content = template.content;
+        
+        while ((child = el.firstChild)) {
+          content.appendChild(child);
+        }
+      }
+    }
     return fragment
   }
 
